bdd7aaf67419198edf55c96b36ada94f1ba0be42
unssfit <unssfit@gmail.com>
2018-02-09 14:41:30 +0000

    work with dictionary

--- theMinionGame.py
+++ theMinionGame.py
@@ -39,11 +39,44 @@ def get_player2_substring():
                     count = 0
     print(player2_words)
 
+def minion_Game():
+    get_player2_substring()
+    get_player1_substring()
+    print('Game Start')
+    # player 1 part
+    p1_counter, p2_counter  = len(player1_words), len(player2_words)
+    count, p1_word, p1_total = 0, [], 0
+    while p1_counter != count:
+        p1 = str(input('Enter the Word:'))
+        if p1 in player1_words:
+            count += 1
+            p1_word.append(p1)
+            p1_total += player1_words[p1]
+            for key in p1_word:
+                print(key, player1_words[key])
+
+        else:
+            print('Wrong Word! Tray Again!',end='')
+    print('Total:',p1_total)
+
+    # player2 part
+    p2_total, p2_word, count2 = 0, [], 0
+    while p2_counter != count2:
+        p2 = str(input('Enter The Word:'))
+        if p2 in player2_words:
+            count2 += 1
+            p2_word.append(p2)
+            p2_total += player2_words[p2]
+            for item in p2_word:
+                print(item, player2_words[item])
+        else:
+            print('Wrong Word Tray Again! ',end='')
+    print('total:', p2_total)   
 
 
 
-get_player2_substring()
-get_player1_substring()
+
+minion_Game()
 
 
 
@@ -52,5 +85,5 @@ get_player1_substring()
 
 
 
-#def minion_Game(string):
+
 
